21910f259613f55afecb0c28fcd144f44bde3aa7
mubarak <mubarakcali24@gmail.com>
2023-05-12 09:43:37 -0700

    seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE company_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
